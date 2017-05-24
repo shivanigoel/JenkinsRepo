0dada450c14d1ba1b6e8b7192f6564ac738ae987
Shivani <Shivani@192.168.0.5>
2017-05-24 23:50:43 +0530

    First GIt

--- learn/src/main/java/com/org/learningmaven/learn/learnall.java
+++ learn/src/main/java/com/org/learningmaven/learn/learnall.java
@@ -3,5 +3,6 @@ package com.org.learningmaven.learn;
 public class learnall {
 public static void main(String[] args) {
 	System.out.println("This is for learnng github");
+	System.out.println("hello");
 }
 }
